51e1a18d447d37d73c85dc4569df9864e21cdc7c
polscode <paulsegales@gmail.com>
2024-11-28 20:14:53 -0500

    Initialize project with Spring Bott and dependencies

---
+++ src/main/java/com/shadow/PayTrackManager/PayTrackManagerApplication.java
@@ -0,0 +1,13 @@
+package com.shadow.PayTrackManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayTrackManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayTrackManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shadow/PayTrackManager/PayTrackManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadow.PayTrackManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayTrackManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
